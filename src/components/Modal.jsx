0f1f27d0d24f2f12170057cc2d4c59a0bcc81554
misterdan100 <danielmca60@gmail.com>
2023-11-11 23:04:07 -0500

    Add form inputs and labels for add expenses in modal component

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -21,6 +21,44 @@ const Modal = ({ setModal, animarModal, setAnimarModal }) => {
 
         <form className={`formulario ${ animarModal ? 'animar' : 'cerrar'}`}>
             <legend>Nuevo Gasto</legend>
+
+            <div className="campo">
+                <label htmlFor="nombre">Nombre Gasto</label>
+                <input
+                    id="nombre"
+                    type="text" 
+                    placeholder='Añadel el Nombre del Gasto'
+                />
+            </div>
+
+            <div className="campo">
+                <label htmlFor="cantidad">Cantidad</label>
+                <input
+                    id="cantidad"
+                    type="text" 
+                    placeholder='Añade el valor ($ 000.00)'
+                />
+            </div>
+
+            <div className="campo">
+                <label htmlFor="categoria">Categoria</label>
+                
+                <select name="" id="id">
+                    <option value="">-- Seleccione --</option>
+                    <option value="ahorro">Ahorro</option>
+                    <option value="comida">Comida</option>
+                    <option value="casa">Casa</option>
+                    <option value="gastos">Gastos Varios</option>
+                    <option value="ocio">Ocio</option>
+                    <option value="salud">Salud</option>
+                    <option value="suscripciones">Suscripciones</option>
+                </select>
+            </div>
+
+            <input
+            type="submit"
+            value='Añadir Gasto'
+            />
         </form>
     </div>
   )
