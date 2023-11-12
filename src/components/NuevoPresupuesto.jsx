a972a15c1ef080b72679be8f994a2ea401ded2fc
misterdan100 <danielmca60@gmail.com>
2023-11-11 21:22:34 -0500

    Creating components, header and principal input with validation to numbers

---
+++ src/components/NuevoPresupuesto.jsx
@@ -0,0 +1,45 @@
+import { React, useState} from 'react'
+import Mensaje from './Mensaje'
+
+const NuevoPresupuesto = ({ presupuesto, setPresupuesto}) => {
+
+    const [ mensaje, setMensaje ] = useState('')
+
+    const handlePresupuesto = (e) => {
+        e.preventDefault();
+        if (!Number(presupuesto) || Number(presupuesto < 0)) {
+            setMensaje('No es un presupesto valido!');
+        } else {
+            setMensaje('Es un presupuesto valido!');
+        }
+    }
+
+  return (
+    <div className='contenedor-presupuesto contenedor sombra'>
+        <form onSubmit={handlePresupuesto} className='formulario'>
+            <div className='campo'>
+                <label >Definir presupuesto $</label>
+                <input
+                    type="text"
+                    className='nuevo-presupuesto'
+                    placeholder='Añade tu Presupuesto'
+                    value={presupuesto}
+                    onChange={ e => setPresupuesto(e.target.value)}
+                />
+            </div>
+
+            <input
+                type="submit"
+                value="Añadir"
+            />
+
+            {mensaje && <Mensaje
+                tipo='error'
+            >{mensaje}</Mensaje>}
+
+        </form>
+    </div>
+  )
+}
+
+export default NuevoPresupuesto
